e5c47d9ff776845c09c1dc727e470f1ca6835293
zachfedor <zachfedor@gmail.com>
2015-07-19 15:10:15 -0400

    setting up node server with http and fs

--- server.js
+++ server.js
@@ -2,4 +2,18 @@
  * server.js
  */
 
-console.log( "node server!" );
+var http = require('http'),
+    fs   = require('fs');
+
+http.createServer( function( req, res ) {
+    res.writeHead( 200, {
+        'Content-Type' : 'text/html',
+        'Access-Control-Allow-Origin' : '*'
+    });
+
+    var readStream = fs.createReadStream( __dirname + '/public/index.html' );
+
+    readStream.pipe( res );
+}).listen( 1337 );
+
+console.log( 'Visit me at http://localhost:1337' );
