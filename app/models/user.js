e1160e89bbfb399dff332285510c01025ec9f949
zachfedor <zachfedor@gmail.com>
2015-07-20 15:57:17 -0400

    creating the user schema

---
+++ app/models/user.js
@@ -0,0 +1,49 @@
+/**
+ * User.js
+ */
+
+// Setup ==============
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt-nodejs');
+
+// Schema =============
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+});
+
+// Password =============
+// hash the password before saving it
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+    });
+});
+
+// compare the password to it's hash
+UserSchema.methods.comparePassword = function(password) {
+    var user = this;
+
+    return bcrypt.comareSync(password, user.password);
+};
+
+// Finalize
+module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -3,15 +3,55 @@
  */
 
 // Setup ================
-var express = require( 'express' ),
+var express = require('express'),
     app = express(),
-    path = require( 'path' );
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    port = process.env.PORT || 8080;
 
-app.get( '/', function( req, res ) {
-    res.sendFile( path.join( __dirname + '/public/index.html' ));
+mongoose.connect('mongodb://localhost:27017/vili');
+
+var User = require('./app/models/user.js');
+
+// App Configuration =====
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \Authorization');
+
+    next();
 });
 
+app.use(morgan('dev'));
+
 // Routes ================
+app.get('/', function(req, res) {
+    // res.sendFile( path.join( __dirname + '/public/index.html' ));
+    res.send('Welcome to the Home Page!');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.use(function(req, res, next) {
+    console.log('somebody is using our app!');
+
+    // more later
+
+    next();
+});
+
+apiRouter.get('/', function(req, res) {
+    res.json({ message: 'hooray!' });
+});
+
+// Register Routes =======
+app.use('/api', apiRouter);
+
+/*
 app.route( '/login' )
     .get( function( req, res ) {
         res.send( 'this is the login form' );
@@ -54,10 +94,10 @@ adminRouter.get( '/posts', function( req, res ) {
 });
 
 app.use( '/admin', adminRouter );
-/*app.use( '/', basicRouter );
+app.use( '/', basicRouter );
 app.use( '/api', apiRoutes );*/
 
 // Finalize ==============
-app.listen( 1337 );
+app.listen( port );
 
-console.log( 'Visit me at http://localhost:1337' );
+console.log( 'Magic is happening on port ' + port );
