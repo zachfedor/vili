929f3083716c271260af707f8975ed86f9bec940
zachfedor <zachfedor@gmail.com>
2015-07-29 16:37:37 -0400

    added login route

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -5,8 +5,16 @@
 angular.module('app.routes', ['ngRoute'])
     .config(function($routeProvider, $locationProvider) {
         $routeProvider
+            // homepage route
             .when('/', {
                 templateUrl: 'views/pages/home.html'
+            })
+
+            // login route
+            .when('/login', {
+                templateUrl: 'views/pages/login.html',
+                controller: 'mainController',
+                controllerAs: 'login'
             });
 
         $locationProvider.html5Mode(true);
