8c7d07cc107b39c1527fab5dbcd4682b00421c87
zachfedor <zachfedor@gmail.com>
2015-08-01 11:10:44 -0400

    adding auth interceptor into httpProvider

--- public/app/app.js
+++ public/app/app.js
@@ -10,4 +10,10 @@ angular.module('vili', [
     'mainCtrl',
     'userCtrl',
     'userService'
-]);
+])
+
+    // app configuration to integrate token into requests
+    .config(function($httpProvider) {
+        // attach our auth interceptor to the http requests
+        $httpProvider.interceptors.push('AuthInterceptor');
+    });
