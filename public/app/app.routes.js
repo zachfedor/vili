7d60c43d5f7353acf33532026ac9a9f830c66db0
zachfedor <zachfedor@gmail.com>
2015-08-03 12:13:31 -0400

    moved angular views into /public/app/

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -7,14 +7,20 @@ angular.module('app.routes', ['ngRoute'])
         $routeProvider
             // homepage route
             .when('/', {
-                templateUrl: 'views/pages/home.html'
+                templateUrl: 'app/views/pages/home.html'
             })
 
             // login route
             .when('/login', {
-                templateUrl: 'views/pages/login.html',
+                templateUrl: 'app/views/pages/login.html',
                 controller: 'mainController',
                 controllerAs: 'login'
+            })
+
+            .when('/users', {
+                templateUrl: 'app/views/pages/users/all.html',
+                controller: 'userController',
+                controllerAs: 'user'
             });
 
         $locationProvider.html5Mode(true);
